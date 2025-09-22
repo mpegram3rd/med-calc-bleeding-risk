012fd554d9a3999f461ae40bddcdba84a3b59e48
Macon Pegram <mpegram@captechconsulting.com>
2025-09-21 21:58:47 -0400

    Clean up and further refactoring to better component isolation.  Beginning work on state management. Interim broken commit to allow me to switch which computer I'm working on.

--- src/calculator/Calculator.tsx
+++ src/calculator/Calculator.tsx
@@ -1,7 +1,20 @@
-export default function Calculator() {
+import type {Dispatch, SetStateAction} from "react";
+
+interface CalculatorProps {
+    setScore: Dispatch<SetStateAction<number>>;
+}
+
+const Calculator: React.FC<CalculatorProps> = ({setScore}) => {
+
+    const changeScore = (changeAmount: number) => {
+        setScore(prevScore => prevScore + changeAmount);
+    }
+
     return (
         <div className="calculator">
-            <h2>Calculator will go here</h2>
+            <h2>Calculator will go here just need to build a table</h2>
         </div>
     );
 }
+
+export default Calculator;
--- src/calculator/CalculatorContainer.tsx
+++ src/calculator/CalculatorContainer.tsx
@@ -1,11 +1,14 @@
 import './CalculatorContainer.css'
 import Advice from './Advice.tsx'
 import Calculator from "./Calculator.tsx";
+import {useState} from 'react';
 
 export default function CalculatorContainer() {
+    const [score, setScore] = useState<number>(0);
+
     return (
         <div className="calc-container">
-            <Calculator/>
+            <Calculator setScore={setScore}/>
             <Advice/>
         </div>
     );
