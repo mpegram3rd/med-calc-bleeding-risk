71afe87744cc23279355a61690c8c730c7e7727e
mpegram3rd <mpergam3rd@hotmail.com>
2025-09-22 21:01:51 -0400

    Much better layout and management of responsiveness

--- src/calculator/CalculatorContainer.tsx
+++ src/calculator/CalculatorContainer.tsx
@@ -2,15 +2,15 @@ import './CalculatorContainer.css'
 import Calculator from "./Calculator.tsx";
 import {useState} from 'react';
 import ResultsContainer from "../results/ResultsContainer.tsx";
+import {SimpleGrid} from "@mantine/core";
 
 export default function CalculatorContainer() {
     const [score, setScore] = useState<number>(0);
 
     return (
-        <div className="calc-container">
+        <SimpleGrid cols={({ base: 1, md: 2 })}>
             <Calculator setScore={setScore}/>
             <ResultsContainer score={score}/>
-        </div>
-
+        </SimpleGrid>
     );
 }
--- src/results/ResultsContainer.tsx
+++ src/results/ResultsContainer.tsx
@@ -1,5 +1,6 @@
 import Advice from "./Advice.tsx";
 import type {ScoreProps} from "./score-props.ts";
+import Reference from "../reference/Reference.tsx";
 
 
 const ResultsContainer: React.FC<ScoreProps> = ({score}) => {
@@ -7,6 +8,7 @@ const ResultsContainer: React.FC<ScoreProps> = ({score}) => {
     return (
         <div className="results-container">
             <Advice score={score}/>
+            <Reference/>
         </div>
     )
 }
